567b2311b3aa79f5025501d07f1a7a0456bbf41d
Patrick McDonough <patrick@prodesigndevelop.com>
2018-12-26 13:57:18 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBjWlc8tZLInLp_C43KGL2fKfpExHr_AnE",
+  authDomain: "cbd4horses-47217.firebaseapp.com",
+  databaseURL: "https://cbd4horses-47217.firebaseio.com",
+  projectId: "cbd4horses-47217",
+  storageBucket: "cbd4horses-47217.appspot.com",
+  messagingSenderId: "321951766420"
+};
+firebase.initializeApp(config);
